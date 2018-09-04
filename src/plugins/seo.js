8cb7764d60b7ff192904f43b5db2ebc2c4cb9ade
jerry.liu <liuhuanhui@live.cn>
2018-09-04 11:04:41 +0800

    fix  https://github.com/nuxt-community/nuxt-i18n/issues/115 （It's not a good way to repair.）

--- src/plugins/seo.js
+++ src/plugins/seo.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
-
+import Cookies from 'js-cookie'
+const localHtmlAttrsCookieKey = 'localHtmlAttrs'
 Vue.mixin({
   head () {
     const COMPONENT_OPTIONS_KEY = '<%= options.COMPONENT_OPTIONS_KEY %>'
+    // issues: https://github.com/nuxt-community/nuxt-i18n/issues/115 （It's not a good way to repair.）
+    // TODO + FIXME:There is another problem. There is no time to solve it. Title is also deleted.
+    let returnData = {}
+    if (Cookies.get(localHtmlAttrsCookieKey)) {
+      returnData = JSON.parse(Cookies.get(localHtmlAttrsCookieKey))
+    }
+    console.log('returnData', returnData)
     if (
       !this._hasMetaInfo ||
       !this.$i18n ||
@@ -10,15 +18,15 @@ Vue.mixin({
       this.$options[COMPONENT_OPTIONS_KEY] === false ||
       (this.$options[COMPONENT_OPTIONS_KEY] && this.$options[COMPONENT_OPTIONS_KEY].seo === false)
     ) {
-      return {};
+      return returnData;
     }
     const LOCALE_CODE_KEY = '<%= options.LOCALE_CODE_KEY %>'
     const LOCALE_ISO_KEY = '<%= options.LOCALE_ISO_KEY %>'
     const BASE_URL = '<%= options.baseUrl %>'
 
     // Prepare html lang attribute
-    const currentLocaleData = this.$i18n.locales.find(l => l[LOCALE_CODE_KEY] === this.$i18n.locale)
     const htmlAttrs = {}
+    const currentLocaleData = this.$i18n.locales.find(l => l[LOCALE_CODE_KEY] === this.$i18n.locale)
     if (currentLocaleData && currentLocaleData[LOCALE_ISO_KEY]) {
       htmlAttrs.lang = currentLocaleData[LOCALE_ISO_KEY]
     }
@@ -63,12 +71,17 @@ Vue.mixin({
           content: locale[LOCALE_ISO_KEY].replace(/-/g, '_')
         }))
     );
-
-    return {
+    returnData = {
       htmlAttrs,
       link,
       meta
     }
+    // save to local
+    const date = new Date()
+    Cookies.set(localHtmlAttrsCookieKey, JSON.stringify(returnData), {
+      expires: new Date(date.setDate(date.getDate() + 365))
+    })
+    return returnData
   }
 })
 
